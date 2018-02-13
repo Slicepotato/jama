499599da6bb00f59d4eb5371dc430fb1c3448879
slicepotato <m.ianjohnson@gmail.com>
2018-02-12 19:37:57 -0500

    full width page template

---
+++ jama-theme/full-width.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Full Width */
+?>
+
+<?php get_header(); ?>
+
+<div id="primary" class="site-content-fullwidth">
+    <main id="main" class="site-main" role="main">
+        <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
