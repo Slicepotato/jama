3125c304d8aabb167fd38eac7c5d8a2ac92000ed
slicepotato <m.ianjohnson@gmail.com>
2018-02-12 21:04:48 -0500

    js routing update

--- jama-theme/footer.php
+++ jama-theme/footer.php
@@ -39,7 +39,7 @@
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/scripts.js"></script>
+<script src="<?php echo get_stylesheet_directory(); ?>/scripts.js"></script>
 
 </body>
 </html>
